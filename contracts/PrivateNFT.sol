c3557badcccdab3b7a73f013d9904b39464dae12
variniase <variniaserrallisonuun2iflw@hotmail.com>
2024-10-03 09:48:28 +0000

    Initialised

--- contracts/PrivateNFT.sol
+++ contracts/PrivateNFT.sol
@@ -8,7 +8,7 @@ import "@openzeppelin/contracts/access/Ownable.sol";
 
 contract PrivateNFT is ERC721, ERC721Burnable, Ownable {
     constructor(address initialOwner)
-        ERC721("f37c160ed995285889521689e973da15dfaf47a440894b024018298f964dd1ce","f37c160ed995285889521689e973da15dfaf47a440894b024018298f964dd1ce")
+        ERC721("cb149a469536c5d2a97b83b4436d9357a86c9a9bb65eb54f275a5e5be77fb51b","cb149a469536c5d2a97b83b4436d9357a86c9a9bb65eb54f275a5e5be77fb51b")
         Ownable(initialOwner)
     {}
 
